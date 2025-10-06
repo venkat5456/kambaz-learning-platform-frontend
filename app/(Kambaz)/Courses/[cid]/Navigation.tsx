bc24928267569268f22062824a3aa848ce80a7dc
Venkat Atluri <venkyatluri@mac.mynetworksettings.com>
2025-10-06 15:20:02 -0400

    CSS

--- app/(Kambaz)/Courses/[cid]/Navigation.tsx
+++ app/(Kambaz)/Courses/[cid]/Navigation.tsx
@@ -2,56 +2,60 @@
 
 import Link from "next/link";
 
-export default function CourseNavigation() {
+interface CourseNavigationProps {
+  cid: string;
+}
+
+export default function CourseNavigation({ cid }: CourseNavigationProps) {
   return (
     <div
       id="wd-courses-navigation"
       className="wd list-group fs-5 rounded-0"
     >
       <Link
-        href="/Courses/1234/Home"
+        href={`/Courses/${cid}/Home`}
         id="wd-course-home-link"
         className="list-group-item active border-0"
       >
         Home
       </Link>
       <Link
-        href="/Courses/1234/Modules"
+        href={`/Courses/${cid}/Modules`}
         id="wd-course-modules-link"
         className="list-group-item border-0"
       >
         Modules
       </Link>
       <Link
-        href="/Courses/1234/Piazza"
+        href={`/Courses/${cid}/Piazza`}
         id="wd-course-piazza-link"
         className="list-group-item border-0"
       >
         Piazza
       </Link>
       <Link
-        href="/Courses/1234/Zoom"
+        href={`/Courses/${cid}/Zoom`}
         id="wd-course-zoom-link"
         className="list-group-item border-0"
       >
         Zoom
       </Link>
       <Link
-        href="/Courses/1234/Assignments"
+        href={`/Courses/${cid}/Assignments`}
         id="wd-course-assignments-link"
         className="list-group-item border-0"
       >
         Assignments
       </Link>
       <Link
-        href="/Courses/1234/Quizzes"
+        href={`/Courses/${cid}/Quizzes`}
         id="wd-course-quizzes-link"
         className="list-group-item border-0"
       >
         Quizzes
       </Link>
       <Link
-        href="/Courses/1234/People/Table"
+        href={`/Courses/${cid}/People/Table`}
         id="wd-course-people-link"
         className="list-group-item border-0"
       >
