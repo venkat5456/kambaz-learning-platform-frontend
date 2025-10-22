34cdcd51e4738076b127108093e5e1059e7ac899
Venkat Atluri <venkyatluri@mac.mynetworksettings.com>
2025-10-22 14:48:21 -0400

    Fix ESLint errors

--- app/Labs/Lab3/VariableTypes.tsx
+++ app/Labs/Lab3/VariableTypes.tsx
@@ -1,12 +1,12 @@
 export default function VariableTypes() {
-  let numberVariable = 123;
-  let floatingPointNumber = 234.345;
-  let stringVariable = "Hello World!";
-  let booleanVariable = true;
+  const numberVariable = 123;
+const floatingPointNumber = 234.345;
+const stringVariable = "Hello World!";
+const booleanVariable = true;
 
-  let isNumber = typeof numberVariable;
-  let isString = typeof stringVariable;
-  let isBoolean = typeof booleanVariable;
+const isNumber = typeof numberVariable;
+const isString = typeof stringVariable;
+const isBoolean = typeof booleanVariable;
 
   return (
     <div id="wd-variable-types" className="p-3">
